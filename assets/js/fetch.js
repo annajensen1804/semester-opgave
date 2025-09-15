6e4f8624326dc3ada956c9133d8948e1fa8cef33
anntrofim-debug <ann.trofim@gmail.com>
2025-09-15 12:32:46 +0200

    fetch

---
+++ assets/js/fetch.js
@@ -0,0 +1,49 @@
+export async function fetchReviews() {
+  try {
+    const response = await fetch("https://glamping-rqu9j.ondigitalocean.app/reviews/");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
+
+export async function fetchStays() {
+  try {
+    const response = await fetch("https://glamping-rqu9j.ondigitalocean.app/stays/");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
+
+export async function fetchActivities() {
+  try {
+    const response = await fetch("https://glamping-rqu9j.ondigitalocean.app/activities/");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
+
+export async function fetchContacts() {
+  try {
+    const response = await fetch("https://glamping-rqu9j.ondigitalocean.app/contacts");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
+
+export async function fetchUsers() {
+  try {
+    const response = await fetch("https://glamping-rqu9j.ondigitalocean.app/users/");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
