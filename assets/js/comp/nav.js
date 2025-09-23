d6b3e02203cf22553e3b56e49f0ed2b724e8c028
anntrofim-debug <ann.trofim@gmail.com>
2025-09-23 10:54:10 +0200

    nav

--- assets/js/comp/nav.js
+++ assets/js/comp/nav.js
@@ -6,11 +6,11 @@ const navTmpl = () => {
             <img src="../assets/images/icons/bars-solid.svg" alt="menu"></img>
         </div>
 
-    <ul class="nav-list"
-      <li class="nav-item"><a href="#ophold" class="nav-link">Ophold</a></li>
-      <li class="nav-item"><a href="#kontakt" class="nav-link">Kontakt</a></li>
-      <li class="nav-item"><a href="#aktiviteter" class="nav-link">Aktiviteter</a></li>
-      <li class="nav-item"><a href="#minListe" class="nav-link">Min liste</a></li>
+    <ul class="nav-list">
+      <li class="nav-item"><a href="ophold.html" class="nav-link">Ophold</a></li>
+      <li class="nav-item"><a href="kontakt.html" class="nav-link">Kontakt</a></li>
+      <li class="nav-item"><a href="aktiviteter.html" class="nav-link">Aktiviteter</a></li>
+      <li class="nav-item"><a href="favoritter.html" class="nav-link">Min liste</a></li>
     </ul>
     `;
 }
