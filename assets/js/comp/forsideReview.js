fcde5d4fd0695bc8272278a2a1352add15b389f6
anntrofim-debug <ann.trofim@gmail.com>
2025-10-02 09:15:37 +0200

    a

--- assets/js/comp/forsideReview.js
+++ assets/js/comp/forsideReview.js
@@ -5,7 +5,7 @@ console.log(reviews);
 
 const frontpageReviewContainer = document.querySelector(".frontpage-review");
 
-frontpageReviewContainer.innerHTML = `<h2 class="h2-rev">Vores gæster udtaler</h2>`;
+
 
 
 const frontpageReviewTmpl = (rev) => {
@@ -20,6 +20,7 @@ const frontpageReviewTmpl = (rev) => {
 
 export const frontpageReview = () => {
   if (frontpageReviewContainer) {
+    frontpageReviewContainer.innerHTML = `<h2 class="h2-rev">Vores gæster udtaler</h2>`;
     reviews.forEach((element) => {
       frontpageReviewContainer.insertAdjacentHTML("beforeend", frontpageReviewTmpl(element));
     });
